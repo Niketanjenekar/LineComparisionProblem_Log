050b39684bd505ddba7d4904412c178e71190c2e
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-18 13:21:58 +0530

    welcome to line comparision problem

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace log_LineComparisionProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            WelcomeToLineComputationProblem welcome = new WelcomeToLineComputationProblem();
+            welcome.WelcometoLineComputation();
+
+
+        }
+    }
+}
---
+++ WelcomeToLineComputationProblem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionComputationProblem
+{
+    internal class WelcomeToLineComputationProblem
+    {
+        public static void WelcometoLineComputation()
+        {
+            Console.WriteLine("Welcome to the Line Computational Problems : ");
+        }
+    }
+}
